ea2b229d2969460f664ad9a929ba96d8c5647d32
willdc4 <williamdavicoelho@gmail.com>
2019-07-03 20:04:30 -0300

    comentarios

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@
 #include <cmath>
 #include <cstdlib>
 #include <ctime>
-#include "mpi.h"
+#include "mpi.h"		// import da biblioteca mpi
 #include "solucao.hpp"
 
 using namespace std;
 
+// Função gera o grafo com as intâncias do aquivo
 Grafo *leArquivo(string nomeArquivo) {
     ifstream arquivo;
     arquivo.open(nomeArquivo.c_str());
@@ -35,8 +36,8 @@ bool melhoraSolucao(Solucao* &solucao, double &probabilidade) {
     vector<double> melhorArestas = solucao->getArestas();
     double melhorSomatorio = solucao->getSomatorioTotal();
     solucao->buscaLocal();
-
-    if (melhorSomatorio <= solucao->getSomatorioTotal()) {
+    // verifica se após a busca local a solução melhorou, e atualiza a solução, arestas e somatório
+    if (melhorSomatorio <= solucao->getSomatorioTotal()) { 
         solucao->setSolucao(melhorSolucao);
         solucao->setArestas(melhorArestas);
         solucao->setSomatorioTotal(melhorSomatorio);
@@ -47,28 +48,31 @@ bool melhoraSolucao(Solucao* &solucao, double &probabilidade) {
     probabilidade = solucao->getSomatorioTotal();
     return true;
 }
-
+// Função que executa os ciclos
 void executa(Solucao *solucao, double limitante, Grafo *grafo, vector<int> &melhorRota,
              vector<double> &melhorArestas, double &melhorSomatorio) {
-    bool exclui = false;
+    bool exclui = false; // booleano para verificar se a solução será excluida
     int qtdIteracoes = 0;
     do {
         double probabilidade = 0, somatorioProbabilidade;
-        if (!melhoraSolucao(solucao, probabilidade)) {
+        if (!melhoraSolucao(solucao, probabilidade)) { // Verifica se a solução foi melhorada
             exclui = true;
         }
+        // verifica se a solução tem o melhor somatório, e atualiza a solução, arestas e somatório
         if (solucao->getSomatorioTotal() < melhorSomatorio) {
             melhorRota = solucao->getSolucao();
             melhorArestas = solucao->getArestas();
             melhorSomatorio = solucao->getSomatorioTotal();
         }
 
-        MPI_Barrier(MPI_COMM_WORLD);
-        MPI_Reduce(&probabilidade, &somatorioProbabilidade, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-        MPI_Bcast(&somatorioProbabilidade, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Barrier(MPI_COMM_WORLD);// Bloqueia ate que todos os processos estejam neste trecho do código
+        MPI_Reduce(&probabilidade, &somatorioProbabilidade, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);// Faz a soma da probabilidade de cada processo em somatorioProbabilidade
+        MPI_Bcast(&somatorioProbabilidade, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);// Faz o broadcast de somatorioProbabilidade para todos os processos
 
+        //Tenta melhorar a solução se o cálculo da probabilidade é maior que  o limitante
         if ((solucao->getSomatorioTotal() / somatorioProbabilidade) > limitante) {
             melhoraSolucao(solucao, probabilidade);
+            // verifica se a solução tem o melhor somatório, e atualiza a solução, arestas e somatório
             if (solucao->getSomatorioTotal() < melhorSomatorio) {
                 melhorRota = solucao->getSolucao();
                 melhorArestas = solucao->getArestas();
@@ -76,23 +80,24 @@ void executa(Solucao *solucao, double limitante, Grafo *grafo, vector<int> &melh
             }
         }
 
-        if (exclui) {
+        if (exclui) { // se exclui == true a fonte (solucao) se esgotou 
             Solucao *solucaoAux;
             solucaoAux = solucao;
-            delete solucaoAux;
+            delete solucaoAux;	
             solucao = new Solucao(grafo);
         }
         ++qtdIteracoes;
     } while (qtdIteracoes < 5);
 }
 
+
 int main(int argc, char **argv) {
     time_t inicio = time(NULL);
     time_t fim;
     srand(time(NULL));
 
-    string nomeArquivo = "a280.txt";
-    Grafo *grafo = leArquivo(nomeArquivo);
+    string nomeArquivo = "a280.txt";		// Entrada do arquivo com as instâncias
+    Grafo *grafo = leArquivo(nomeArquivo); // Gera o grafo com as instancias do aquivo
 
     Solucao *melhorSolucao = new Solucao(grafo);
     vector<int> melhorRota = melhorSolucao->getSolucao();
@@ -107,33 +112,34 @@ int main(int argc, char **argv) {
     // inicializa o MPI
     MPI::Init(argc, argv);
 
-    int tamanho = MPI::COMM_WORLD.Get_size();
-    int processo = MPI::COMM_WORLD.Get_rank();
+    int tamanho = MPI::COMM_WORLD.Get_size(); // Quantidade de processos criados
+    int processo = MPI::COMM_WORLD.Get_rank(); // rank (id) do processo
 
-    if (processo == 0) {
-        limitante = 1.0 / tamanho;
+    if (processo == 0) { // Verifica se é o primeiro processo
+        limitante = 1.0 / tamanho;	// Calcula o limitante 
     }
 
-    MPI_Barrier(MPI_COMM_WORLD);
-    MPI_Bcast(&limitante, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD); // Bloqueia ate que todos os processos estejam neste trecho do código
+    MPI_Bcast(&limitante, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);// Faz o broadcast do limitante para todos os processos
 
-    for (int i = 0; i < tamanho; ++i) {
+    for (int i = 0; i < tamanho; ++i) { // Gera uma fonte (solucao) para cada processo
         if (processo == i) {
-            Solucao *solucao = new Solucao(grafo);
+            Solucao *solucao = new Solucao(grafo); 
             executa(solucao, limitante, grafo, melhorRota, melhorArestas, melhorSomatorio);
         }
     }
     
-    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD);// Bloqueia ate que todos os processos estejam neste trecho do código
+    // melhorSomatorioGeral recebe o menor dentre o melhorSomatorio de todos os processos
     MPI_Reduce(&melhorSomatorio, &melhorSomatorioGeral, 1, MPI_DOUBLE, MPI_MIN, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&melhorSomatorioGeral, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&melhorSomatorioGeral, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);// Faz o broadcast do melhorSomatorioGeral para todos os processos
 
-    if (melhorSomatorio == melhorSomatorioGeral) {
+    if (melhorSomatorio == melhorSomatorioGeral) { // Faz broadcast da melhorRota do processo que tem a melhor solução
         MPI_Bcast(&melhorRota, melhorRota.size(), MPI_INT, processo, MPI_COMM_WORLD);
     }
 
-    MPI_Barrier(MPI_COMM_WORLD);
-    if (processo == 0) {
+    MPI_Barrier(MPI_COMM_WORLD); // Bloqueia ate que todos os processos estejam neste trecho do código
+    if (processo == 0) {	// Verifica se é o primeiro processo
         cout << "\n\nRota: ";
         for (unsigned int i = 0; i < melhorRota.size(); ++i) {
             cout << melhorRota[i] << " ";
@@ -144,7 +150,7 @@ int main(int argc, char **argv) {
         cout << "Tempo de execução em segundos: " << difftime(fim, inicio) << endl;
     }
     
-    MPI::Finalize();
+    MPI::Finalize();// Encerra o mpi
     
     delete grafo;
 
